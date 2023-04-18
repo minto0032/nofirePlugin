dcc329e871d43c74d2d0bd0224164aa35ba4b0c1
minto0032 <102743684+minto0032@users.noreply.github.com>
2023-04-19 00:47:43 +0900

    Add files via upload

--- src/main/java/com/github/minto0032/nofireplugin/NofirePlugin.java
+++ src/main/java/com/github/minto0032/nofireplugin/NofirePlugin.java
@@ -7,6 +7,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 public class NofirePlugin extends JavaPlugin implements Listener {
 
@@ -19,22 +21,41 @@ public class NofirePlugin extends JavaPlugin implements Listener {
     public void onDisable() {
     }
 
+    public static boolean enabled = true;
+
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
         Location location = event.getBlock().getLocation();
         Material blockType = event.getBlock().getType();
 
-        if (blockType == Material.FIRE) {
-            // 2ブロック以内に他のプレイヤーがいたら火を消す
-            for (Player otherPlayer : location.getWorld().getPlayers()) {
-                if (otherPlayer.getLocation().distance(location) <= 2.0) {
-                    if (!otherPlayer.equals(player)) {
-                        event.setCancelled(true);
-                        player.sendMessage("§4他のプレイヤーの近くに火をつけることはできません！");
+        if (enabled) {
+            if (blockType == Material.FIRE) {
+                // 2ブロック以内に他のプレイヤーがいたら火を消す
+                for (Player otherPlayer : location.getWorld().getPlayers()) {
+                    if (otherPlayer.getLocation().distance(location) <= 2.0) {
+                        if (!otherPlayer.equals(player)) {
+                            event.setCancelled(true);
+                            player.sendMessage("§4他のプレイヤーの近くに火をつけることはできません！");
+                        }
                     }
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args[0].equals("on")){
+            sender.sendMessage("オンにしました");
+            enabled = true;
+            return true;
+        }
+        if(args[0].equals("off")){
+            sender.sendMessage("オフにしました");
+            enabled = false;
+            return true;
+        }
+        return true;
     }
 }
